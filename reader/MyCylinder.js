af0dead544d6ffdfb457556c7289e0011d3617c4
Ines <gomes.inesisabel@gmail.com>
2016-10-19 01:02:29 +0100

    cilindro com normais (estão ao contrário rsrsrsrs) cilindro com tampas.

--- reader/MyCylinder.js
+++ reader/MyCylinder.js
@@ -19,14 +19,11 @@
 
  	var incAng = 2*Math.PI/this.slices;
  	var incHeight = this.height / this.stacks;
- 	var incRadius = (this.top/2 - this.base/2) / this.stacks;
+ 	var incRadius = (this.top - this.base) / this.stacks; //acho que não se divide por 2
 
  	this.vertices = [];
-
  	this.indices = [];
-
  	this.normals = [];
-
  	this.texCoords = [];
 
  	for(var i = 0; i < this.stacks+1;i++)
@@ -37,35 +34,37 @@
  	                      (this.base + incRadius*i)*Math.sin(incAng*j),
  	                      -this.height/2 + incHeight*i);
 
- 	   // this.texCoords.push(1-1/this.stacks*i,1-1/this.slices*j);
- 	    this.texCoords.push(j/(this.slices), 1 -i/this.stacks);
+ 	    this.texCoords.push(j/this.slices,i/this.stacks); // nao percebo
+ 	    	
+ 	    //calculo das normais : produto vetorial do vetor tangente com o vetor da superficie
+ 	    var v1 = [-Math.sin(incAng*j),Math.cos(incAng*j),0]; //tangente
+        var v2 = [(this.base-this.top)*Math.cos(incAng*j), (this.base-this.top)*Math.sin(incAng*j), this.height]; //vetor da superficie
 
- 	    this.normals.push(Math.cos(incAng*j),
- 	                      0,                        //alterar
- 	                      Math.sin(incAng*j))
+        var norm = [v1[1]*v2[2] - v1[2]*v2[1], v1[2]*v2[0] - v1[0]*v2[2], v1[0]*v2[1] - v1[1]*v2[0]];
+	 
+ 	    this.normals.push(norm[0],norm[1],norm[2]);
 
- 	    if(j > 0 && i > 0)
- 	    {
-          
-          this.indices.push(i*(this.slices+1)+j,
-                            i*(this.slices+1)+(j-1),
-                            (i-1)*(this.slices+1)+(j-1)); //0,4,1 (stacks = 1,slices 3)
+ 	    if(j != this.slices && i != this.stacks)
+ 	    {       
+ 	        this.indices.push(i*(this.slices+1)+j,
+ 	                          (i+1)*(this.slices+1)+j,
+                              i*(this.slices+1)+j+1); //0,4,1 (stacks = 1,slices 3)
 
-         this.indices.push((i)*(this.slices+1)+j,
-                            (i-1)*(this.slices+1)+(j-1),
-                            (i-1)*(this.slices+1)+j); //4,5,1*/
+            this.indices.push((i+1)*(this.slices+1)+j,
+                              (i+1)*(this.slices+1)+j+1,
+                               i*(this.slices+1)+j+1); //4,5,1*/
  	    }
  	  }
  	}
 
- 	//this.newCircle(this.height,1);
- 	//this.newCircle(,-1);
+ 	this.newCircle(this.height,1,(this.slices+1)*(this.stacks));
+ 	this.newCircle(-this.height,-1,(this.slices+1)*(this.stacks)+2);
  	
  	this.primitiveType = this.scene.gl.TRIANGLES;
  	this.initGLBuffers();
  };
-/*
-MyCylinder.prototype.newCircle = function(z, z_normal) {
+
+MyCylinder.prototype.newCircle = function(z, z_normal,index) {
 
     var ang = Math.PI * 2 / this.slices;
     
@@ -74,20 +73,15 @@ MyCylinder.prototype.newCircle = function(z, z_normal) {
     this.texCoords.push(0.5,0.5);
     this.normals.push(0, 0, z_normal);
 
-    for (var i = 0; i < this.slices; i++) {
+    for (var i = 0; i < this.slices; i++) 
+    {
         this.texCoords.push(0.5+Math.cos(ang*i)/2,0.5-Math.sin(ang*i)/2);
         this.vertices.push(Math.cos(ang * i), Math.sin(ang * i), z);
         this.normals.push(0, 0, z_normal);
         
-        if (i % 2 != 0) {// a cada 2 vertices  
-            this.indices.push(0, i, i + 1);
-        } 
-        if(i% 2 == 0){
-             this.indices.push(0, i, i + 1);
-        }
-        if (i == this.slices - 1) {
-          this.indices.push(0, i+1, 1);
-            //último triangulo
+        if (i > 0) {
+          this.indices.push(index, index+ i+1, index+1);
+            
         }
     }    
-}*/
+}
