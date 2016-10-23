ae882159e0966107e91a14226e1ed66a6db34e7e
Ines <gomes.inesisabel@gmail.com>
2016-10-23 22:17:21 +0100

    My color entre 0 e 1 e uma correção de valores no dsx

--- reader/MyColor.js
+++ reader/MyColor.js
@@ -6,7 +6,7 @@
  *  a - alpha component (transparency)
  */
 function MyColor(r,g,b,a) {
-  if(r < 0 || r > 255 || g < 0 || g >255 || b < 0 || b > 255 || a < 0 || a > 255)
+  if(r < 0 || r > 1 || g < 0 || g >1 || b < 0 || b > 1 || a < 0 || a > 1)
     return "color values to high or low";
  	this.r = r;
  	this.g = g; 
